f6d1dc2134794fd78792b5f04c91f3d3eff7d4a4
FeliKanenga <www.felicia.kanenga@umuzi.org>
2020-04-06 16:56:32 +0200

    first commit

---
+++ src/main/java/GameOfLife.java
@@ -0,0 +1,99 @@
+public class GameOfLife {
+    public static void main(String[] args)
+    {
+        int M = 10, N = 10;
+
+        // The grid.
+        int[][] grid = {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1 },
+                { 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0 }
+        };
+
+        // showing/printing the grid
+        System.out.println("First Grid");
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (grid[i][j] == 0)
+                    System.out.print("+");
+                else
+                    System.out.print("#");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        newGrid(grid, M, N);
+    }
+
+    // to get a new Grid
+    static void newGrid(int grid[][], int x, int y)
+    {
+        int[][] future = new int[x][y];
+
+        // Loop through cells
+        for (int a = 1; a < x - 1; a++)
+        {
+            for (int b  = 1; b  < y - 1; b ++)
+            {
+                int aliveNeighbours = 0;
+                for (int i = -1; i <= 1; i++)
+                    for (int j = -1; j <= 1; j++)
+                        aliveNeighbours += grid[a + i][b  + j];
+
+                aliveNeighbours -= grid[a][b ];
+
+
+                if ((grid[a][b ] == 1) && (aliveNeighbours < 2))
+                    future[a][b ] = 0;
+
+
+                else if ((grid[a][b ] == 1) && (aliveNeighbours > 3))
+                    future[a][b ] = 0;
+
+
+                else if ((grid[a][b ] == 0) && (aliveNeighbours == 3))
+                    future[a][b ] = 1;
+
+
+                else
+                    future[a][b ] = grid[a][b ];
+            }
+        }
+
+        System.out.println("Next Grid");
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                if (future[i][j] == 0)
+                    System.out.print("+");
+                else
+                    System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+    private CellState state;
+
+    public void Cell(CellState state){
+
+        this.state = state;
+    }
+
+    public CellState GetNextState(int i) {
+        return i > 1  && i < 4 ? CellState.ALIVE : CellState.DEAD;
+    }
+
+    public enum CellState {
+        ALIVE, DEAD
+    }
+}
